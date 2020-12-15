74e3d9cfe07f5c843c3fc79ab79de028322664c9
athet <illia.nesterenko@nure.ua>
2020-12-15 18:51:26 +0200

    tmp

---
+++ script.js
@@ -0,0 +1,7 @@
+    let modal = document.getElementById('buy');
+
+    let btn = document.getElementById('buy_button');
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
